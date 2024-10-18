e199b44b783a9720caebd7bcfabdb44840f70f17
Tedla Tesfaye <t.godebo@alustudent.com>
2024-10-18 11:04:43 +0200

    corrected

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 number = 98
-teda = number
-print(f"{teda} Battery street")
+print("{:d}Battery street".format(number))
--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-nu = round(number, 2)
-print(f"Float: {nu}")
+print(f"Float: {number:.2f}")
