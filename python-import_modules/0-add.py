7f04b7616b481322c2fe48ee828097840b4ee890
Tedla Tesfaye <t.godebo@alustudent.com>
2024-10-22 15:31:54 +0200

    corrected

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add
+from add_0 import add
 def main():
-    print("1 + 2 = ""{:d}".format(add.add(1, 2)))
+    print("1 + 2 = ""{:d}".format(add(1, 2)))
 
 if __name__ == "__main__":
     main()
