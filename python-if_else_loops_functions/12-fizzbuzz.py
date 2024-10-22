93e9a22971a010d72ba266d2fe9791ee9b7a707e
Tedla Tesfaye <t.godebo@alustudent.com>
2024-10-22 12:41:51 +0200

    fizz buzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz(i):
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz ", end="")
+        elif i % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print(f"{i} ", end="")
+        
+
+fizzbuzz(9)
