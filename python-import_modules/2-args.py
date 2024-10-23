b258a13d20921b6110f9c3c2f4330d0cd646dafb
Tedla Tesfaye <t.godebo@alustudent.com>
2024-10-23 12:00:56 +0200

    argument exercise

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    if argc == 0:
+        print("{:d} arguments.".format(argc))
+    elif argc == 1:
+        print("{:d} argument:".format(argc))
+    else:
+        print("{:d} arguments:".format(argc))
+    for i in range(1, len(argv)):  
+        print("{:d}: {}".format(i, argv[i]))
+ 
+    
