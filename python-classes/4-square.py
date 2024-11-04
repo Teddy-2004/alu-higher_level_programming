af54acfc39cc99fecf8ce83bac7a03819e506a3f
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-04 12:08:49 +0200

    getter and setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """added getter and setter"""
 
 
@@ -12,15 +11,12 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-
     def area(self):
         return self.__size ** 2
-    
 
     @property
     def size(self):
         return self.__size
-    
 
     @size.setter
     def size(self, value):
