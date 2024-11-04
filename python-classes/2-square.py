1578e459ee37a00306b48d54e150f9aadd43426e
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-04 10:25:59 +0200

    raised exception on class

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """class raises an error"""
     def __init__(self, size=0):
-        if type(size) is not int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
