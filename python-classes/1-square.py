40dc5f0ba267cbb770e3c576477540ce1f421323
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-04 09:45:02 +0200

    created a class with private instance attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This file creates a class of Square with private instance attribute"""
+
+
+class Square:
+    """This class creates a class of private instance attribute"""
+    def __init__(self, size):
+        self.__size = size
