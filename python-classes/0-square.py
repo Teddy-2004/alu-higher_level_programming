5198165413ad737be9a6f2f63de6a76cc2b19dd9
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-04 09:20:24 +0200

    first task done

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """This task is all about creating an empty class"""
+
+
 class Square:
-    """What this class does is just creating an empty class maybe for later use"""
+    """What this class does is just creating
+      an empty class maybe for later use"""
     pass
