6a3c4f1d2639c7478a77018daf5928eab5d48d05
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-04 10:27:07 +0200

    raised exception on class

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-"""private instant attribute with exceptions"""
+
+"""
+This module defines a Square class.
+"""
 
 
 class Square:
-    """class raises an error"""
+    """
+    A class used to represent a Square.
+    """
+
     def __init__(self, size=0):
+        """
+        Initialize the square, and validate.
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size__ = size
+        self.__size = size
