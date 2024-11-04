b56c74449d500483be9d4cd7d47a4267d3eec3b6
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-04 10:35:38 +0200

    area of the size created

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    A class used to represent a Square.
+    """
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+
+    def area(self):
+        return self.__size * self.__size
