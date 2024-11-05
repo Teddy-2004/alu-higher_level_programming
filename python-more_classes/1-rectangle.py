36629e6bd5da41e97b5004dd4c4806276e85235c
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-05 10:05:25 +0200

    width and height added

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
-"""Creating empty class"""
+"""
+
+Class Rectangle that defines a rectangle
+
+"""
 
 
 class Rectangle:
-    """creating private instance attributes"""
+    """
+    Create Rectangle class
+    """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """
+        Initialize variables
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
+        """
+        returns width value
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        sets width value
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -22,10 +37,16 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+        returns height value
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        sets width value
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
