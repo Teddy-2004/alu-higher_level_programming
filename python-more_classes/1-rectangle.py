00572a551e7d36f82d029d9eabdd1868124f7f64
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-05 10:00:28 +0200

    width and height added

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
-    
+
     @property
     def width(self):
         return self.__width
@@ -16,18 +16,18 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
