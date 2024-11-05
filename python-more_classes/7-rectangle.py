6618372710e50aa356f61385fcd59d31c7470633
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-05 11:24:04 +0200

    public class attribute print_symbol added

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -5,6 +5,7 @@ Class Rectangle that defines a rectangle
 
 """
 
+
 class Rectangle:
     """
     Create Rectangle class
@@ -12,7 +13,6 @@ class Rectangle:
 
     number_of_instances = 0
     print_symbol = "#"
-
 
     def __init__(self, width=0, height=0):
         """
@@ -70,7 +70,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) 
+        return "\n".join([str(self.print_symbol)
                           * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
