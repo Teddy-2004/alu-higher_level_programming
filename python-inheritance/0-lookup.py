95cef7b0f28a789552d5a8cc86f9616767db2aff
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-06 13:32:23 +0200

    readme and lookup created

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A function that returns the avialable
+attribute and method
+"""
+def lookup(obj):
+    """
+    when called this function returs attributes
+    and objects
+    """
+    return dir(obj)
