a9d9caf10705d0bfdd306c3c8387c138e58c217b
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-07 11:54:55 +0200

    created drived class

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+class Rectangle that inherits BaseGeometry
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle"""
+    def __init__(self, width, height):
+        """ initialize """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ area """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ str """
+        string = "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        return string
+
+
+class Square(Rectangle):
+    """ square """
+    def __init__(self, size):
+        """ initialize """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
