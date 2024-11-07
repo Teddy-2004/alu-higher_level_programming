10a25111155786a7ced31cf8e3d3e2f84e025ce8
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-07 10:18:59 +0200

    object is an instance of the class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+checks if the object is an instance of a class 
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    this fun checks if obj is instance of the class
+    """
+    return type(obj) is a_class
