4d61cb18652458abea06cd77be3b55756a2a5b0a
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-07 10:31:05 +0200

    is in a class or it's inheritance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+checks if an object of either an instance of
+class or it's inheritance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    is an object is in class
+    """
+    return isinstance(obj, a_class)
