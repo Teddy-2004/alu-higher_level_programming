2d7668ee8dd4744610a999eaf6434eb5eda52817
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-07 11:23:15 +0200

    exception raised

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+public instance method
+"""
+
+
+class BaseGeometry:
+    """creats a method to raise an exception"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
