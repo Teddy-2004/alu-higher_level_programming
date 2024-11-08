d0856328d67e33d4d3076d2e00b7ca96b0f390a1
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-08 10:34:09 +0200

    done with additional tasks

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" class MyInt that inherits from int """
+
+
+class MyInt(int):
+    """
+    this class inherites from int
+    """
+
+    def __eq__(self, other):
+        """ equals change to not equals """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """ not equals to equals """
+        return super().__eq__(other)
