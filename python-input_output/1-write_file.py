77f63d613a18c0fa2f447f0502f08a2454888aff
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-13 12:54:31 +0200

    counting fun created

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -9,9 +9,6 @@ def write_file(filename="", text=""):
     count lines in file
     """
     count = 0
-    if filename == "":
-        return count
-    with open(filename, "r") as f:
-        for l in f:
-            count += 1
-    return count
+    with open(filename, "w", encoding="utf-8") as f:
+        linenum = f.write(text)
+        return linenum
