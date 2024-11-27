5dce73d04742bc69cfa4f78cb0759fdb364dc285
Tedla Tesfaye <t.godebo@alustudent.com>
2024-11-27 14:57:42 +0200

    task done

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in URL, sends a request, displays size of the body of response
-curl -sI "$@" | grep -i Content-Length | awk '{print $2}'
+# Uses curl on the first argument and prints the size of it.
+curl -s "$1" | wc -c
